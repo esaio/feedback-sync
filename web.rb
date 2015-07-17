07362cbb0a3b982d3fb9a0a14d05fbbb8d1ea9ec
fukayatsu <fukayatsu@gmail.com>
2015-07-17 18:31:56 +0900

    Fix NoMethodError

--- web.rb
+++ web.rb
@@ -15,6 +15,9 @@ post '/webhook' do
   # Do nothing unless reply
   return unless subject.to_s.match(/^Re:/)
 
+  redis   = Redis.new(url: ENV['REDIS_URL'])
+  octokit = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+
   # Extract feedback_id from subject
   feedback_id = subject.scan(/\(FB-(\d+)\)/).flatten.first.to_i
 
@@ -22,11 +25,10 @@ post '/webhook' do
   feedback_content = body.split(/^\d{4}.\d{1,2}.\d{1,2}.+:$/).first.strip
 
   # Get issue number
-  redis = Redis.new(url: ENV['REDIS_URL'])
   redis_key = "feedbacks:#{feedback_id}:issue_number"
   issue_number = redis.get(redis_key)
+
   unless issue_number
-    octokit = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
     issue = octokit.search_issues("[FB-#{feedback_id}] in:title repo:fukayatsu/esa author:aridori", sort: 'created', order: 'asc').items.first
     issue_number = issue.number
     redis.set(redis_key, issue_number)
